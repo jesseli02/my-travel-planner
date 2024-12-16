d894f790e550baba47cd824d9ac4a759ee4372f5
Jesse <lijesse01@gmail.com>
2024-12-15 19:31:57 -0500

    Test 2

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 from together import Together
+import json
 
 # Initialize Together AI client
 client = Together(api_key="cbea512d1bf322aee99d7ce57605f76213a88036512f376396654844eba7efe8")
 
 # Key user inputs
-start_date = "2024-03-19"
-end_date = "2024-03-23"
-start_time = "noon"
-end_time = "noon"
-start_location = "Tulum, Mexico"
-end_location = "Tulum, Mexico"
-daily_budget = "200 CAD per person per day"
-vibe = "relaxing, eat, and enjoy some food"
-travelers = "7"
-travel_speed = "slow to average"
-additional_info = "We're going to be 3 couples and 1 single person"
+user_input = {
+    "arrival_date": "2024-03-19",
+    "departure_date": "2024-03-23",
+    "arrival_time": "noon",
+    'departure_time': 'noon',
+    'start_location': 'Tulum, Mexico',
+    'end_location': 'Tulum, Mexico',
+    'daily_budget': '200 CAD per person per day',
+    'vibe': 'relaxing, eat, and enjoy some food',
+    'number_of_travelers': '7',
+    'travel_speed': 'slow to average',
+    'additional_info': "We're going to be 3 couples and 1 single person"
+}
 
-# Crafting prompt for AI
-prompt = (
-    f"Hi, I would like to some help planning a travel itinerary - could you help plan a daily itinerary for me. "
-    f"Here are some details about my travel plan: We will be arriving at {start_location} and departing at {end_location} on {start_date} and {end_date}. "
-    f"We're going to be in total {travelers} people and {travel_speed} travelers. "
-    f"We will be traveling with the intention of {vibe}. We will be budgeting for {daily_budget}. "
-    f"In addition, {additional_info}."
-)
+# Crafting request message for AI
+prompt = "Could you help me plan a daily itinerary for my upcoming trip? Here are the details below: "
+trip_info = json.dumps(user_input)
 
+# Calling Together AI model
 stream = client.chat.completions.create(
   model = "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
-  messages = [{"role": "user", "content": prompt}],
+  messages = [{"role": "user", "content": (prompt + trip_info)}],
   max_tokens = 1000,
   stream = True
 )
