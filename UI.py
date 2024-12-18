e2511a51df517bdb2fd696ccc90210174a72f265
jesse <lijesse01@gmail.com>
2024-12-18 09:38:12 -0500

    added UI file

--- UI.py
+++ UI.py
@@ -2,4 +2,19 @@ import json
 import streamlit as st
 import pandas as pd
 
-text_input = st.text_input("Arrival date", "Jan 01, 2024")
+user_input = []
+
+text_questions = [
+    "Start location",
+    "End location",
+    'Vibe',
+    'Must-try activities',
+    'Interests',
+    'Additional information that could be helpul'
+]
+
+for question in text_questions:
+    answer = st.text(question)
+    user_input.append({question:answer})
+
+print(user_input)
