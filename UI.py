642d66606adea3a9735bc295c525b642f8e89e97
jesse <lijesse01@gmail.com>
2024-12-19 09:31:03 -0500

    added UI file

--- UI.py
+++ UI.py
@@ -2,19 +2,53 @@ import json
 import streamlit as st
 import pandas as pd
 
-user_input = []
+# Create master list of questions
 
-text_questions = [
-    "Start location",
-    "End location",
-    'Vibe',
-    'Must-try activities',
-    'Interests',
-    'Additional information that could be helpul'
+question_headers = ['QuestionName', 'QuestionText', 'PlaceholderText', 'InputType', 'Requirement']
+questions = [
+    ['Arrival location', 'Where are you starting your first day?', 'Brisbane, Australia', 'text', 'mandatory'],
+    ['Departure location', 'Where are you leaving on your last day?', 'Melbourne, Australia', 'text', 'mandatory'],
+    ['Arrival date', 'What date if your first day?', None , 'date', 'mandatory'],
+    ['Departure date', 'What date is your last day?', None, 'date', 'mandatory'],
+    ['Arrival time', 'What time do you start on your first day', None, 'time', 'optional'],
+    ['Departure time', 'What time do you end on your last day', None, 'time', 'optional'],
+    ['Travelers', 'How many people are you traveling with including yourself?', '2 couples so 4 people in total', 'text', 'mandatory'],
+    ['Activities in mind', 'Are there any activities/sites on your list that you want to visit', 'Sydney Opera House, Bondi Beach, Visit the Gold Coast', 'text', 'optional'],
+    ['Travel style', 'Can you share anything about your travel style?', 'I try to avoid places where there are long line ups', 'text', 'optional'],
+    ['Interests', 'What are your general interests when traveling?', 'I enjoy walking around, trying new foods and exploring the culture', ' text', 'optional'],
+    ['Trip Budget', 'Do you have a daily budget in mind', 'We plan to $2000 CAD total, excluding flights', 'text', 'optional'],
+    ['Additional info', 'Is there anything else that would be good for know to help plan your trip itinerary?', 'I am a morning person', 'text', 'optional']
 ]
 
-for question in text_questions:
-    answer = st.text_input(question)
-    user_input.append({question:answer})
+questions_table = pd.DataFrame(questions, columns=question_headers)
 
-print(user_input)
+
+input_details = []
+
+# Translate question table to UI on Streamlit
+for question in questions_table:
+    match question['InputType']:
+        case 'text':
+            answer = st.text_input(
+                label = question['QuestionText'],
+                placeholder = ['PlaceholderText']
+            )
+        case 'date':
+            answer = st.date_input(
+                label = question['QuestionText']
+            )
+        case 'time':
+            answer = st.time_input(
+                label = question['QuestionText']
+            )
+        case _:
+            answer = None
+
+    input_details.append({'question': question['QuestionName'], 'answer': answer})
+
+# Print out user input details as summary
+
+
+
+
+
