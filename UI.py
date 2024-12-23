81409699abe9571df4bbd9cad6eae0530fef270a
jesse <lijesse01@gmail.com>
2024-12-23 13:04:43 -0500

    added UI file

--- UI.py
+++ UI.py
@@ -15,47 +15,45 @@ questions = [
     ['Travelers', 'How many people are you traveling with including yourself?', '2 couples, so 4 people in total', 'text', 'mandatory'],
     ['Activities in mind', 'Are there any activities/sites on your list that you want to visit', 'Sydney Opera House, Bondi Beach, See the Gold Coast', 'text', 'optional'],
     ['Travel style', 'Can you share anything about your travel style?', 'I try to avoid places where there are long line ups', 'text', 'optional'],
-    ['Interests', 'What are your general interests when traveling?', 'I enjoy walking around, trying new foods and exploring the culture', ' text', 'optional'],
+    ['Interests', 'What are your general interests when traveling?', 'I enjoy walking around, trying new foods and exploring the culture', 'text', 'optional'],
     ['Trip Budget', 'Do you have a daily budget in mind', 'We plan to spend $2000 CAD total per person, excluding flights', 'text', 'optional'],
-    ['Additional info', 'Is there anything else that would be good for know to help plan your trip itinerary?', 'I am a morning person', 'text', 'optional']
+    ['Additional info', 'Is there anything else that would be good to know to help plan your trip itinerary?', 'I am a morning person', 'text', 'optional']
 ]
 questions_table = pd.DataFrame(questions, columns=question_headers)
 
 # Translate question table to UI on Streamlit
 input_details = []
 
-for question in questions_table.to_dict(orient ='records'):
-    match question['InputType']:
-        case 'text':
-            answer = st.text_input(
+with st.form(key = 'submission_form')
+    key = 'submission_form',
+
+    for question in questions_table.to_dict(orient ='records'):
+        input_type = question['InputType']
+
+        if input_type == 'text':
+            answer == st.text_input(
                 label = question['QuestionText'],
-                placeholder = 'E.g., ' + question['PlaceholderText']
+                placeholder = question['PlaceholderText']
             )
-        case 'date':
+        elif input_type == 'date':
             answer = st.date_input(
-                label = question['QuestionText']
+                label=question['QuestionText']
             )
-        case 'time':
+        elif input_time == 'time':
             answer = st.time_input(
-                label = question['QuestionText'],
-                step = 1800
+                label=question['QuestionText'],
+                step=1800
             )
-        case _:
+        else:
             answer = None
 
-    input_details.append({'question': question['QuestionName'], 'answer': answer})
+        input_details.append({'question': question['QuestionName'], 'answer': answer})
 
-# Create button UI to a generate itinerary
-iteration = 0
-def refresh(n):
-    return n + 1
+    submitted = st.form_submit_button("Unleash the power of AI to generate my travel itinerary")
 
-st.button(
-    label = "Unleash the power of AI to generate my travel itinerary",
-    type = 'primary'
-)
-st.write(iteration)
-st.write(input_details)
+if submitted:
+    st.write("Here's what I gathered from your travel input details:")
+    st.table(input_details)
 
 # Button action to begin
 
